ed0f3e8adc57306db722f8f94d97d11ec407fd87
tandw <654333035@qq.com>
2017-06-21 07:51:16 +0800

    1.修改样式

---
+++ Train/Engine.TCMS.Turkmenistan/Src/Engine.TCMS.Turkmenistan/Converter/DoubleToStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Engine.TCMS.Turkmenistan.Converter
+{
+    public class DoubleToStringConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value==null||value==DependencyProperty.UnsetValue)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            if (value.GetType()==typeof(double))
+            {
+                var tmp = (double)value;
+                return tmp.ToString("F0");
+            }
+            return value;
+
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
